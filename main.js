33ca08153d357d692975a49dddccde9e5b848ffe
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-05-09 21:52:42 -0400

    Add typed js design using javascript

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Full Stack Developer",
+    "Web Developer",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
